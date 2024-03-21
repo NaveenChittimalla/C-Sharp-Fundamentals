8ef05fb1824c832df061c2ca40943438f0266950
NaveenChittimalla <naveenjits431@gmail.com>
2024-03-21 20:22:56 +0530

    MyConsoleHelperLibrary

---
+++ src/MyConsoleHelperSolution/MyConsoleHelper.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyConsoleHelper.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string value = MyConsoleHelperLibrary.ReadInput.ReadTextInput();
+            //Console.WriteLine("Text Value:" + value);
+            //int iValue = MyConsoleHelperLibrary.ReadInput.ReadIntegerInput();
+            //Console.WriteLine("Integer Value:" + iValue);
+
+            //string valueG = MyConsoleHelperLibrary.ReadInput.ReadInputValue<string>();
+            //Console.WriteLine("G Text Value:" + valueG);
+            //int iValueG = MyConsoleHelperLibrary.ReadInput.ReadInputValue<int>();
+            //Console.WriteLine("G Integer Value:" + iValueG);
+
+            int age = MyConsoleHelperLibrary.ReadInput.ReadInputValue<int>("Enter Age", "Invalid Age");
+            Console.WriteLine("Age :" + age);
+            age = MyConsoleHelperLibrary.ReadInput.ReadInputValue<int>("Enter Age"
+                , "Age must be between 18 and 100"
+                , (value) => value > 18 && value < 100);
+
+            Console.WriteLine("age limit:" + age);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/MyConsoleHelperSolution/MyConsoleHelperLibrary/ReadInput.cs
@@ -0,0 +1,129 @@
+﻿using System.Data;
+
+namespace MyConsoleHelperLibrary
+{
+    public static class ReadInput
+    {
+        /*
+        * Usage:
+            string value = MyConsoleHelperLibrary.ReadInput.ReadTextInput();
+            string firstName = MyConsoleHelperLibrary.ReadInput.ReadTextInput("Enter FirstName");
+            string firstName = MyConsoleHelperLibrary.ReadInput.ReadTextInput("Enter FirstName","Invalid FirstName.");
+        */
+        public static string ReadTextInput(string prompt = null, string inValidMessage = null)
+        {
+            string inputedValue = string.Empty;
+            bool isValid;
+            prompt = prompt ?? "Enter input value";
+            inValidMessage = inValidMessage ?? "Invalid input. Please enter valid text.";
+            do
+            {
+                Console.Write($"{prompt}:");
+                inputedValue = Console.ReadLine();
+                isValid = !string.IsNullOrEmpty(inputedValue)
+                    && !string.IsNullOrWhiteSpace(inputedValue)
+                    && inputedValue.Trim() != string.Empty;
+                if (!isValid)
+                {
+                    Console.WriteLine(inValidMessage);
+                }
+            } while (!isValid);
+
+            return inputedValue;
+        }
+
+        /*
+        * Usage:
+            int iValueG = MyConsoleHelperLibrary.ReadInput.ReadIntegerInput();
+            int age = MyConsoleHelperLibrary.ReadInput.ReadTextInput("Enter Age");
+            int age = MyConsoleHelperLibrary.ReadInput.ReadTextInput("Enter Age","Invalid Age.");
+        */
+        public static int ReadIntegerInput(
+            string prompt = null,
+            string inValidMessage = null)
+        {
+            int value;
+            bool isValid;
+            prompt = prompt ?? "Enter input value";
+            inValidMessage = inValidMessage ?? "Invalid input. Please enter valid integer.";
+            do
+            {
+                Console.Write($"{prompt}:");
+                string inputedValue = Console.ReadLine();
+                isValid = int.TryParse(inputedValue, out value);
+                if (!isValid)
+                {
+                    Console.WriteLine(inValidMessage);
+                }
+            } while (!isValid);
+
+            return value;
+        }
+
+        /*
+         * Usage:
+             string value = MyConsoleHelperLibrary.ReadInput.ReadInputValue<string>();
+             int value = MyConsoleHelperLibrary.ReadInput.ReadInputValue<int>();
+             int value = MyConsoleHelperLibrary.ReadInput.ReadInputValue<int>("Enter Age");
+             int value = MyConsoleHelperLibrary.ReadInput.ReadInputValue<int>("Enter Age", "Invalid Age");
+             int value = MyConsoleHelperLibrary.ReadInput.ReadInputValue<int>("Enter Age",
+                           "Age must be between 18 and 100", (value) => value > 18 && value < 100);
+         */
+        public static T ReadInputValue<T>(
+            string prompt = null,
+            string invalidMessage = null,
+            Predicate<T> validationFn = null)
+        {
+            T value = default;
+            bool isValid = false;
+            bool isValidValidationFn = false;
+            prompt ??= $"Enter input value ({typeof(T).Name})";
+            invalidMessage ??= $"Invalid input. Please enter valid {typeof(T).Name} value.";
+            validationFn ??= v => true;
+
+            do
+            {
+                Console.Write($"{prompt}:");
+                string inputedValue = Console.ReadLine();
+                switch (typeof(T).Name)
+                {
+                    case "String":
+                        isValid = !string.IsNullOrEmpty(inputedValue)
+                                    && !string.IsNullOrWhiteSpace(inputedValue)
+                                    && inputedValue.Trim() != string.Empty;
+                        value = (T)(object)inputedValue;
+                        break;
+                    case "Int32":
+                        isValid = int.TryParse(inputedValue, out int intValue);
+                        value = (T)(object)intValue;
+                        break;
+                    case "Int64":
+                        isValid = long.TryParse(inputedValue, out long longValue);
+                        value = (T)(object)longValue;
+                        break;
+                    case "Decimal":
+                        isValid = decimal.TryParse(inputedValue, out decimal decimalValue);
+                        value = (T)(object)decimalValue;
+                        break;
+                    case "Double":
+                        isValid = double.TryParse(inputedValue, out double doubleValue);
+                        value = (T)(object)doubleValue;
+                        break;
+                    case "Single": //Float
+                        isValid = float.TryParse(inputedValue, out float floatValue);
+                        value = (T)(object)floatValue;
+                        break;
+                    default:
+                        Console.WriteLine($"{typeof(T).Name} not supported" );
+                        break;
+                }
+                isValidValidationFn = validationFn(value);
+                if (!isValid || !isValidValidationFn)
+                {
+                    Console.WriteLine(invalidMessage);
+                }
+            } while (!isValid || !isValidValidationFn);
+            return value;
+        }
+    }
+}
